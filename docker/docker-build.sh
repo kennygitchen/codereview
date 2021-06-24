530ca5bd387fa7c18a75e739ac6ded0d449f2b9b
Kenny Chen <kc1230@hotmail.com>
2021-06-24 13:09:18 +1000

    make docker-build.sh more secure

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,7 +1,7 @@
 DOCKER_REG=351762539201.dkr.ecr.us-east-1.amazonaws.com
 DOCKER_REPO=$DOCKER_REG/codereview
 GIT_SHA=$(git rev-parse --short HEAD)
-docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) $DOCKER_REG
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DOCKER_REG
 docker build . -f docker/Dockerfile -t codereview-jar:$GIT_SHA -t codereview-jar:latest
 docker tag codereview-jar:$GIT_SHA $DOCKER_REPO:$GIT_SHA
 docker tag codereview-jar:latest $DOCKER_REPO:latest
